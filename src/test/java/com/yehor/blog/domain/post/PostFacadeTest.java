41861f22d6b507c4f081708cedbf03793be18a3d
yehor1959 <zaitsev.yehor@gmail.com>
2026-01-25 21:12:09 +0100

    Add PostFacadeTest

---
+++ src/test/java/com/yehor/blog/domain/post/PostFacadeTest.java
@@ -0,0 +1,25 @@
+package com.yehor.blog.domain.post;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PostFacadeTest {
+
+    @Test
+    public void should_save_4_posts_when_there_are_no_posts_in_database() {
+        // given
+//        OfferFacade offerFacade = new OfferFacadeTestConfiguration(List.of()).offerFacadeForTests();
+
+        // when
+//        offerFacade.saveOffer(new OfferRequestDto("id", "asds", "asdasd", "1"));
+//        offerFacade.saveOffer(new OfferRequestDto("id", "asds", "asdasd", "2"));
+//        offerFacade.saveOffer(new OfferRequestDto("id", "asds", "asdasd", "3"));
+//        offerFacade.saveOffer(new OfferRequestDto("id", "asds", "asdasd", "4"));
+
+        // then
+//        assertThat(offerFacade.findAllOffers()).hasSize(4);
+    }
+}
